04d522907bb602211070e8572ebd078d45304b45
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2019-12-12 16:08:25 +0100

    added a header component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,40 @@
-import React from 'react';
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { countReducer } from './state/reducers';
-import Counter from './components/counter';
+import React from "react";
+import styled from "styled-components";
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { countReducer } from "./state/reducers";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
 
 const monsterReducer = combineReducers({
-  count: countReducer,
-})
+  count: countReducer
+});
 
 export const store = createStore(
   monsterReducer,
   {},
   compose(
     applyMiddleware(thunk /* ,etc , other middlewares */),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export function App() {
   return (
-    <div className="App">
-      Hi
+    <Container>
+      <Header />
       <Counter />
-    </div>
+    </Container>
   );
 }
 
 export default App;
+
+// Styling here:
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
